38a96798a624a01cafdefe9cb162fb35cc0f833f
bienvenudev <cbienvenu007@gmail.com>
2025-11-05 12:17:49 +0200

    feat: implement product editing and deletion features, enhance cart functionality, and update product display (my solution for displaying carts on lesson 16 on 9th module)

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,10 @@ router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
 
+router.get("/products/:productId", shopController.getProduct);
+
+router.post("/cart", shopController.postCart);
+
 router.get("/cart", shopController.getCart);
 
 router.get("/orders", shopController.getOrders);
