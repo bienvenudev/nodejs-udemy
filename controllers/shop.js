7d367f4888d1bf8b9494dd33c3e7300e1bcaa99a
bienvenudev <cbienvenu007@gmail.com>
2025-11-14 15:29:22 +0200

    feat: implement database connection with MySQL and dotenv integration (end of 10th module)

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,57 +2,69 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
+  Product.fetchAll()
+    .then(([products]) => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "All Products",
+        path: "/products",
+      });
+    })
+    .catch((err) => {
+      console.error("error retrieving data in db. Error: ", err);
     });
-  });
 };
 
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
 
-  Product.findById(productId, (product) => {
+  Product.findById(productId).then(([product]) => {
+    console.log('product from id is', product[0])
     res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
+      product: product[0],
+      pageTitle: product[0].title,
       path: "/products",
     });
-  });
+  }).catch(err => {
+    console.error('error while finding by id. Error: ', err)
+  })
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/index", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
+  Product.fetchAll()
+    .then(([products]) => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => {
+      console.error("error retrieving data in db. Error: ", err);
     });
-  });
 };
 
 exports.getCart = (req, res, next) => {
   Cart.getCart((cart) => {
-    Product.fetchAll((products) => {
-      const cartProducts = [];
-      for (let product of products) {
-        const cartProductData = cart.products.find(
-          (prod) => prod.id === product.id
-        );
+    Product.fetchAll();
+    // Product.fetchAll((products) => {
+    //   const cartProducts = [];
+    //   for (let product of products) {
+    //     const cartProductData = cart.products.find(
+    //       (prod) => prod.id === product.id
+    //     );
 
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
+    //     if (cartProductData) {
+    //       cartProducts.push({ productData: product, qty: cartProductData.qty });
+    //     }
+    //   }
 
-      res.render("shop/cart", {
-        pageTitle: "Your Cart",
-        products: cartProducts,
-        path: "/cart",
-      });
-    });
+    //   res.render("shop/cart", {
+    //     pageTitle: "Your Cart",
+    //     products: cartProducts,
+    //     path: "/cart",
+    //   });
+    // });
   });
 };
 
---
+++ util/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  database: process.env.DB_NAME || "node-complete",
+  password: process.env.DB_PASSWORD,
+});
+
+module.exports = pool.promise();
