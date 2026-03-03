4c1eecc2839d506aef339622bfb9961bcf2ebbff
bienvenudev <cbienvenu007@gmail.com>
2026-03-03 14:03:38 +0200

    feat: update cart functionality to handle product quantity and deletion more efficiently

--- controllers/shop.js
+++ controllers/shop.js
@@ -83,8 +83,8 @@ exports.postCart = (req, res, next) => {
         const oldQuantity = product.cartItem.quantity;
         newQuantity = oldQuantity + 1;
         fetchedCart.addProduct(product, {
-          through: { quantity: newQuantity }
-        })
+          through: { quantity: newQuantity },
+        });
       }
       return Product.findByPk(productId)
         .then((product) => {
@@ -102,10 +102,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findById(productId, (product) => {
-    Cart.deleteProduct(productId, product.price);
-    res.redirect("/cart");
-  });
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
